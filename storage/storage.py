92f457876b00f231b191f5fcaee5e6b5626c4788
Adarsh Agrawal <adarsh.agrawal1@ibm.com>
2025-05-07 19:38:57 +0530

    Removing slot number from the payload

--- storage/storage.py
+++ storage/storage.py
@@ -9,31 +9,8 @@ logger = common.get_logger("storage")
 
 CONTENT_TYPE = "application/vnd.ibm.powervm.uom+xml; Type=VirtualIOServer"
 
-def populate_payload(vios_payload, hmc_host, partition_uuid, system_uuid, physical_volume_name, slot):
-    physical_vol_slot = f'''
-    <VirtualSCSIMapping schemaVersion="V1_0">
-            <Metadata>
-                <Atom/>
-            </Metadata>
-            <AssociatedLogicalPartition kb="CUR" kxe="false" href="https://{hmc_host}/rest/api/uom/ManagedSystem/{system_uuid}/LogicalPartition/{partition_uuid}" rel="related"/>
-            <ClientAdapter kb="CUR" kxe="false" schemaVersion="V1_0">
-                <Metadata>
-                    <Atom/>
-                </Metadata>
-                <AdapterType kxe="false" kb="ROR">Client</AdapterType>
-                <VirtualSlotNumber kxe="false" kb="COD">{slot}</VirtualSlotNumber>
-            </ClientAdapter>
-            <Storage kxe="false" kb="CUR">
-                <PhysicalVolume schemaVersion="V1_0">
-                    <Metadata>
-                        <Atom/>
-                    </Metadata>
-                    <VolumeName kb="CUR" kxe="false">{physical_volume_name}</VolumeName>
-                </PhysicalVolume>
-            </Storage>
-        </VirtualSCSIMapping>
-'''
-    physical_vol_no_slot = f'''
+def populate_payload(vios_payload, hmc_host, partition_uuid, system_uuid, physical_volume_name):
+    scsi_mapping = f'''
     <VirtualSCSIMapping schemaVersion="V1_0">
             <Metadata>
                 <Atom/>
@@ -49,22 +26,18 @@ def populate_payload(vios_payload, hmc_host, partition_uuid, system_uuid, physic
             </Storage>
         </VirtualSCSIMapping>
 '''
-    if slot == -1:
-        physical_vol = physical_vol_no_slot
-    else:
-        physical_vol = physical_vol_slot
-    phys_bs = BeautifulSoup(physical_vol, 'xml')
+    phys_bs = BeautifulSoup(scsi_mapping, 'xml')
     vios_bs = BeautifulSoup(vios_payload, 'xml')
     scsi_mappings = vios_bs.find('VirtualSCSIMappings')
     scsi_mappings.append(phys_bs)
     payload = str(vios_bs)
     return payload
 
-def attach_storage(vios_payload, config, cookies, partition_uuid, system_uuid, vios_uuid, slot, physical_vol_name):
+def attach_storage(vios_payload, config, cookies, partition_uuid, system_uuid, vios_uuid, physical_vol_name):
     uri = f"/rest/api/uom/ManagedSystem/{system_uuid}/VirtualIOServer/{vios_uuid}"
     hmc_host = util.get_host_address(config)
     url =  "https://" +  hmc_host + uri
-    payload = populate_payload(vios_payload, hmc_host, partition_uuid, system_uuid, physical_vol_name, slot)
+    payload = populate_payload(vios_payload, hmc_host, partition_uuid, system_uuid, physical_vol_name)
     headers = {"x-api-key": util.get_session_key(config), "Content-Type": CONTENT_TYPE}
     response = requests.post(url, headers=headers, cookies=cookies, data=payload, verify=False)
 
