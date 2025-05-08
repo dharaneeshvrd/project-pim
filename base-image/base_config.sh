e4ed707ab9378b18a80823a37d52af1f0028dce3
Dharaneeshwaran Ravichandran <dharaneeshwaran.ravichandran@ibm.com>
2025-05-08 09:44:48 -0400

    Rename config service and add logs

--- base-image/base_config.sh
+++ base-image/base_config.sh
@@ -4,6 +4,7 @@ set -x
 
 mkdir -p /tmp/pim
 
+echo "Mounting cloud-init ..."
 mount -t iso9660 -o loop /dev/sr1 /tmp/pim
 if [ ! -e /tmp/pim/99_custom_network.cfg ]; then
     umount /tmp/pim
@@ -11,6 +12,7 @@ if [ ! -e /tmp/pim/99_custom_network.cfg ]; then
     mkdir /tmp/pim
     mount -t iso9660 -o loop /dev/sr0 /tmp/pim
 fi
+echo "Cloud-init mounted successfully"
 
 mkdir -p /etc/pim
 cp /tmp/pim/99_custom_network.cfg /etc/cloud/cloud.cfg.d/
@@ -18,3 +20,6 @@ cp /tmp/pim/pim_config.json /etc/pim/
 cp /tmp/pim/auth.json /etc/pim/
 
 echo REGISTRY_AUTH_FILE=/etc/pim/auth.json >> /etc/pim/env.conf
+
+echo "base_config.sh run successfully!"
+
