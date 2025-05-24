5cc1d8cba379f8fc03bf278fd0730a4e8f935bdb
Dharaneeshwaran Ravichandran <dharaneeshwaran.ravichandran@ibm.com>
2025-05-23 21:52:27 -0400

    Configure vllm to use cache dir

--- examples/vllm/llm_config.sh
+++ examples/vllm/llm_config.sh
@@ -2,7 +2,16 @@
 
 set -x
 
-echo LLM_ARGS=$(jq -r '.llmArgs' /etc/pim/pim_config.json) > /etc/pim/env.conf
-echo LLM_IMAGE=$(jq -r '.llmImage' /etc/pim/pim_config.json) >> /etc/pim/env.conf
-echo REGISTRY_AUTH_FILE=/etc/pim/auth.json >> /etc/pim/env.conf
+LLM_ENV=$(jq -r '.llmEnv' /etc/pim/pim_config.json)
+IFS=',' read -ra envs <<< "$LLM_ENV"
+for env in "${envs[@]}"; do
+    echo "$env" >> /etc/pim/env.conf
+done
 
+echo LLM_ARGS=$(jq -r '.llmArgs' /etc/pim/pim_config.json) >> /etc/pim/env.conf
+echo LLM_IMAGE=$(jq -r '.llmImage' /etc/pim/pim_config.json) >> /etc/pim/env.conf
+
+mkdir /var/huggingface
+echo HF_HUB_CACHE=/var/huggingface >> /etc/pim/env.conf
+
+echo REGISTRY_AUTH_FILE=/etc/pim/auth.json >> /etc/pim/env.conf
