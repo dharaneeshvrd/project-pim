92bb25e16e8877146527389e121c55e04bd1926b
Dharaneeshwaran Ravichandran <dharaneeshwaran.ravichandran@ibm.com>
2025-05-26 07:13:33 -0400

    Check env exists already before adding it

--- examples/vllm/llm_config.sh
+++ examples/vllm/llm_config.sh
@@ -2,16 +2,24 @@
 
 set -x
 
+[ -f /etc/pim/llm.conf ] || touch /etc/pim/llm.conf
+
 LLM_ENV=$(jq -r '.llmEnv' /etc/pim/pim_config.json)
 IFS=',' read -ra envs <<< "$LLM_ENV"
 for env in "${envs[@]}"; do
-    echo "$env" >> /etc/pim/env.conf
+    key="${env%%=*}"
+    key="$(echo "$key" | sed 's/^[[:space:]]*//')"
+    sed -i "/^${key}=.*/d" /etc/pim/llm.conf && echo "$(echo "$env" | sed 's/^[[:space:]]*//')" >> /etc/pim/llm.conf
 done
 
-echo LLM_ARGS=$(jq -r '.llmArgs' /etc/pim/pim_config.json) >> /etc/pim/env.conf
-echo LLM_IMAGE=$(jq -r '.llmImage' /etc/pim/pim_config.json) >> /etc/pim/env.conf
+
+var_to_add=LLM_ARGS=$(jq -r '.llmArgs' /etc/pim/pim_config.json)
+sed -i "/^LLM_ARGS=.*/d" /etc/pim/llm.conf && echo "$var_to_add" >> /etc/pim/llm.conf
+
+var_to_add=LLM_IMAGE=$(jq -r '.llmImage' /etc/pim/pim_config.json)
+sed -i "/^LLM_IMAGE=.*/d" /etc/pim/llm.conf && echo "$var_to_add" >> /etc/pim/llm.conf
 
 mkdir /var/huggingface
-echo HF_HUB_CACHE=/var/huggingface >> /etc/pim/env.conf
+var_to_add=HF_HUB_CACHE=/var/huggingface
+sed -i "/^HF_HUB_CACHE=.*/d" /etc/pim/llm.conf && echo "$var_to_add" >> /etc/pim/llm.conf
 
-echo REGISTRY_AUTH_FILE=/etc/pim/auth.json >> /etc/pim/env.conf
