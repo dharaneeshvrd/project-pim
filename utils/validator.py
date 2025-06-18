e3a692f617606a7d60305cd11faf522b0d7beea7
Dharaneeshwaran Ravichandran <Dharaneeshwaran.Ravichandran@ibm.com>
2025-06-18 10:57:49 +0530

    Revert "Adding verification of the host configuration (#45)"
    This reverts commit c51f18c04a73b91afe2b6d84ac0d3325bf791893.

--- utils/validator.py
+++ utils/validator.py
@@ -2,14 +2,12 @@ import json
 import re
 import requests
 
-from bs4 import BeautifulSoup
 from network.virtual_network import get_network_uuid
 
 from .common import *
 from .string_util import *
 
 logger = get_logger("validator")
-
 
 def validate_config(config):
     is_mandatory_param_valid = validate_mandatory_params(config)
@@ -18,9 +16,8 @@ def validate_config(config):
 
     return all([is_mandatory_param_valid, is_digit_param_valid, is_param_value_valid])
 
-
 def validate_rollback_config(config):
-    params_validators = [
+    params_validators= [
         validate_ip_addresses,
         validate_ssh_keys,
     ]
@@ -28,11 +25,10 @@ def validate_rollback_config(config):
     if get_ai_app_request(config) == "yes":
         params_validators.append(validate_ai_app_validator)
 
-    return all(validator(config) for validator in params_validators)
-
+    return all ( validator(config) for validator in params_validators)
 
 def validate_upgrade_config(config):
-    params_validators = [
+    params_validators= [
         validate_ip_addresses,
         validate_ssh_keys,
         validate_auth_json,
@@ -41,8 +37,7 @@ def validate_upgrade_config(config):
     if get_ai_app_request(config) == "yes":
         params_validators.append(validate_ai_app_validator)
 
-    return all(validator(config) for validator in params_validators)
-
+    return all ( validator(config) for validator in params_validators)
 
 def validate_mandatory_params(config):
     parameter_list = [
@@ -55,7 +50,7 @@ def validate_mandatory_params(config):
         # AI parameter validations
         (get_bootstrap_iso_download_url, "ai.bootstrap-iso-url"),
         (get_workload_image, "ai.workload-image"),
-
+        
         # Partition parameter validations
         (get_partition_name, "partition.name"),
         (get_partition_flavor, "partition.flavor"),
@@ -75,16 +70,14 @@ def validate_mandatory_params(config):
     is_valid = True
     for get_param_value, param_name in parameter_list:
         if get_param_value(config) == "":
-            logger.error(
-                f"validation failed: '{param_name}' must not be empty")
+            logger.error(f"validation failed: '{param_name}' must not be empty")
             is_valid = False
 
     return is_valid
 
-
 def validate_digit_params(config):
     flavor_name = get_flavor_name(config)
-    parameter_list = [
+    parameter_list =  [
         (get_network_prefix_length, "partition.network.ip.prefix-length"),
         # Memory validations
         (get_required_disk_size, "partition.storage.size"),
@@ -97,43 +90,35 @@ def validate_digit_params(config):
     cpu_params = []
     if has_dedicated_proc(config) == "true":
         cpu_params = [
-            (get_desired_proc,
-             f"{flavor_name}.cpu.dedicated.desired-proc-unit"),
+            (get_desired_proc, f"{flavor_name}.cpu.dedicated.desired-proc-unit"),
             (get_max_proc, f"{flavor_name}.cpu.dedicated.max-proc-unit"),
             (get_min_proc, f"{flavor_name}.cpu.dedicated.min-proc-unit")
         ]
     else:
         cpu_params = [
-            (get_shared_desired_proc,
-             f"{flavor_name}.cpu.shared.desired-proc-unit"),
+            (get_shared_desired_proc, f"{flavor_name}.cpu.shared.desired-proc-unit"),
             (get_shared_max_proc, f"{flavor_name}.cpu.shared.max-proc-unit"),
             (get_shared_min_proc, f"{flavor_name}.cpu.shared.min-proc-unit"),
-            (get_shared_desired_virt_proc,
-             f"{flavor_name}.cpu.shared.desired-virt-proc"),
-            (get_shared_max_virt_proc,
-             f"{flavor_name}.cpu.shared.max-virt-proc"),
-            (get_shared_min_virt_proc,
-             f"{flavor_name}.cpu.shared.min-virt-proc")
+            (get_shared_desired_virt_proc, f"{flavor_name}.cpu.shared.desired-virt-proc"),
+            (get_shared_max_virt_proc, f"{flavor_name}.cpu.shared.max-virt-proc"),
+            (get_shared_min_virt_proc, f"{flavor_name}.cpu.shared.min-virt-proc")
         ]
     parameter_list.extend(cpu_params)
     parameter_list.extend(memory_params)
-
+        
     is_valid = True
     for get_digit, param_name in parameter_list:
         if not get_digit(config).isdigit():
-            logger.error(
-                f"validation failed: '{param_name}' must be an integer")
+            logger.error(f"validation failed: '{param_name}' must be an integer")
             is_valid = False
-
+    
     for get_memory_param, param_name in memory_params:
         value = get_memory_param(config)
         if value.isdigit() and int(value) % 2 != 0:
-            logger.error(
-                f"validation failed: '{param_name}' must be an even integer")
+            logger.error(f"validation failed: '{param_name}' must be an even integer")
             is_valid = False
 
     return is_valid
-
 
 def validate_params_value(config):
     params_validators = [
@@ -155,8 +140,7 @@ def validate_params_value(config):
     if get_ai_app_request(config) == "yes":
         params_validators.append(validate_ai_app_validator)
 
-    return all(validator(config) for validator in params_validators)
-
+    return all ( validator(config) for validator in params_validators)
 
 def validate_networks(config, cookies, system_uuid):
     network_validator = [
@@ -164,8 +148,7 @@ def validate_networks(config, cookies, system_uuid):
         validate_virtual_switch_name
     ]
 
-    return all(validator(config, cookies, system_uuid) for validator in network_validator)
-
+    return all( validator(config, cookies, system_uuid) for validator in network_validator )
 
 def validate_ip_addresses(config):
     pattern = r"^((25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)\.){3}(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)$"
@@ -182,23 +165,19 @@ def validate_ip_addresses(config):
             is_valid = False
     return is_valid
 
-
 def validate_prefix_length(config):
     value = get_network_prefix_length(config)
     if value.isdigit() and int(value) <= 32:
         return True
-    logger.error(
-        "validation failed: 'partition.network.ip.prefix-length' value must be less then or equal to 32.")
+    logger.error("validation failed: 'partition.network.ip.prefix-length' value must be less then or equal to 32.")
 
-
+# Validating partition is in correct format
 def validate_partition_name(config):
     pattern = r"^[A-Za-z0-9_\.]{1,79}$"
     result = re.match(pattern, get_partition_name(config))
     if not bool(result):
-        logger.error(
-            f"validation failed: 'partition.name' value '{get_partition_name(config)}' have invalid format.")
+        logger.error(f"validation failed: 'partition.name' value '{get_partition_name(config)}' have invalid format.")
     return bool(result)
-
 
 def validate_partition_flavor(config):
     value = get_partition_flavor(config)
@@ -207,63 +186,50 @@ def validate_partition_flavor(config):
     possible_values.extend(defined_flavor_list)
     if value in possible_values:
         return True
-    logger.error(
-        f"validation failed: 'partition.flavor' must filled with {possible_values}.")
+    logger.error(f"validation failed: 'partition.flavor' must filled with {possible_values}.")
     return False
-
 
 # Validating memory parameter:  Memory should be greater then or equal to 32GB
 def validate_desired_memory(config):
     value = get_desired_memory(config)
     if value.isdigit() and int(value) >= 32:
         return True
-    logger.error(
-        f"validation failed: '{get_flavor_name(config)}.memory.desired-memory' must have greater then or equal to 32 GB of memory")
+    logger.error(f"validation failed: '{get_flavor_name(config)}.memory.desired-memory' must have greater then or equal to 32 GB of memory")
     return False
-
 
 def validate_cpu_mode(config):
     value = get_cpu_mode(config)
-    possible_values = ["dedicated", "shared"]
+    possible_values = [ "dedicated", "shared"]
     if value in possible_values:
         return True
-    logger.error(
-        f"validation failed: '{get_flavor_name(config)}.cpu.mode' must filled with {possible_values}")
+    logger.error(f"validation failed: '{get_flavor_name(config)}.cpu.mode' must filled with {possible_values}")
 
-
-# Validating cpu parameter: Minimum requiment for the desired cpu must be greater or equal to 1
+# Validating cpu parameter: Minimum requiment for the desired cpu must be greater or equal to 1 
 def validate_dedicated_desired_proc(config):
     value = get_desired_proc(config)
     if value.isdigit() and int(value) >= 1:
         return True
-    logger.error(
-        f"validation failed: '{get_flavor_name(config)}.cpu.dedicated.desired-proc-unit' must be greater then or equal to 1 proc-unit")
+    logger.error(f"validation failed: '{get_flavor_name(config)}.cpu.dedicated.desired-proc-unit' must be greater then or equal to 1 proc-unit")
     return False
-
 
 # A System Name must contain 1-31 characters. The only special characters that are disallowed are: ( ) < > * $ & ? | [ ] ' " `
 def validate_system_name(config):
     pattern = r"^[^\(\)<>\*\$\&\?|\[\]\'\"\`]{1,31}$"
     result = re.match(pattern, get_system_name(config))
     if not bool(result):
-        logger.error(
-            f"validation failed: 'system.name' value '{get_system_name(config)}' have invalid format.")
+        logger.error(f"validation failed: 'system.name' value '{get_system_name(config)}' have invalid format.")
     return bool(result)
-
 
 def validate_url(url):
     pattern = "^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$"
     return re.match(pattern, url)
 
-
 # Validate the correctness of the bootstrap URL format
 def validate_bootstrap_url(config):
     result = validate_url(get_bootstrap_iso_download_url(config))
     if not bool(result):
-        logger.error(
-            f"validation failed: 'ai.bootstrap-iso-url' value '{get_bootstrap_iso_download_url(config)}' must be a url")
+        logger.error(f"validation failed: 'ai.bootstrap-iso-url' value '{get_bootstrap_iso_download_url(config)}' must be a url")
     return bool(result)
-
 
 # Validate ssh key section
 def validate_ssh_keys(config):
@@ -303,40 +269,33 @@ def validate_virtual_switch_name(config, cookies, system_uuid):
 
     return found
 
-
 def validate_json(value, field):
     if value != "":
         try:
             json.loads(value)
             return True
         except Exception as e:
-            logger.error(
-                f"validation failed: '{field}' must have valid json value. {e}")
+            logger.error(f"validation failed: '{field}' must have valid json value. {e}")
             return False
     return True
-
 
 def validate_auth_json(config):
     return validate_json(get_auth_json(config), "ai.auth-json")
 
-
 def validate_pim_config_json(config):
     return validate_json(get_pim_config_json(config), "ai.pim-config-json")
-
 
 def validate_ai_app_validator(config):
     result = True
 
     url = get_ai_app_url(config)
     if not bool(validate_url(url)):
-        logger.error(
-            f"validation failed: 'ai.validation.url' value '{url}' must be a url")
+        logger.error(f"validation failed: 'ai.validation.url' value '{url}' must be a url")
         result = False
 
     method = get_ai_app_method(config)
     if method != "GET" and method != "POST":
-        logger.error(
-            f"validation failed: 'ai.validation.method' value '{method}' must be either GET or POST")
+        logger.error(f"validation failed: 'ai.validation.method' value '{method}' must be either GET or POST")
         result = False
 
     payload = get_ai_app_payload(config)
@@ -346,78 +305,10 @@ def validate_ai_app_validator(config):
     headers = get_ai_app_headers(config)
     if headers != "" and not validate_json(headers, "ai.validation.headers"):
         result = False
-
+    
     return result
 
-
 def get_flavor_name(config):
-    if has_custom_flavor(config):
+     if has_custom_flavor(config):
         return "custom-flavor"
-    return get_partition_flavor(config)
-
-
-def validate_host_config(config, cookies, sys_uuid):
-    return validate_mngt_console_version(config, cookies) and validate_lmb_size(config, cookies, sys_uuid)
-
-
-def validate_mngt_console_version(config, cookies):
-    uri = "/rest/api/uom/ManagementConsole"
-    url = "https://" + util.get_host_address(config) + uri
-    headers = {"x-api-key": util.get_session_key(config)}
-    try:
-        response = requests.get(url, headers=headers,
-                                cookies=cookies, verify=False)
-        if response.status_code != 200:
-            logger.error(
-                f"failed to get management console version, error: {response.text}")
-            raise Exception(
-                f"failed to get management console version, error: {response.text}")
-
-        soup = BeautifulSoup(response.text, "xml")
-        version_tag = soup.find("VersionInfo")
-        if version_tag == None:
-            logger.error("failed to find version of management console.")
-            raise Exception("failed to find version of management console.")
-        power_version = version_tag.find("Version")
-        service_pack_name = version_tag.find("ServicePackName")
-
-        if int(power_version.text) < 10:
-            logger.error("minimum power version '10' is expected")
-            raise Exception("minimum power version '10' is expected")
-        if int(service_pack_name.text) < 1050:
-            logger.error("minimum firmware version '1050' is expected")
-            raise Exception("minimum firmware version '1050' is expected")
-    except Exception as e:
-        raise e
-
-    return True
-
-
-def validate_lmb_size(config, cookies, sys_uuid):
-    uri = f"/rest/api/uom/ManagedSystem/{sys_uuid}"
-    url = "https://" + util.get_host_address(config) + uri
-    headers = {"x-api-key": util.get_session_key(config)}
-    try:
-        response = requests.get(url, headers=headers,
-                                cookies=cookies, verify=False)
-        if response.status_code != 200:
-            logger.error(
-                f"failed to get management system details, error: {response.text}")
-            raise Exception(
-                f"failed to get management system details, error: {response.text}")
-
-        soup = BeautifulSoup(response.text, "xml")
-        lmb_size = soup.find("CurrentLogicalMemoryBlockSize")
-        if lmb_size is None:
-            logger.error("failed to find CurrentLogicalMemoryBlockSize value")
-            raise Exception("failed to find CurrentLogicalMemoryBlockSize value.")
-
-        if int(lmb_size.text) < 1024:
-            logger.error(
-                "insufficient memory for CurrentLogicalMemoryBlockSize, atleast '1024 MB' is expected")
-            raise Exception(
-                "insufficient memory for CurrentLogicalMemoryBlockSize, atleast '1024 MB' is expected.")
-    except Exception as e:
-        raise e
-
-    return True
+     return get_partition_flavor(config)
